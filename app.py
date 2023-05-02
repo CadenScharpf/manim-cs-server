8615c01a1de67670124896b88de84f6b090a8c53
Caden Scharpf <u16scharpf@gmail.com>
2023-05-02 15:28:56 -0700

    add getFile endpoint

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from manimcs import ManimCsEngine
 
 
 app = Flask(__name__)
+_output_dir = os.path.join(os.getcwd(), 'output')
 
+@app.route('/getfile', methods=['GET'])
+def return_files_tut():
+    json_data = request.get_json()
+    if not 'file' in json_data:
+        return "Invalid request"
+    file = json_data['file']
+    sp = file.split('/')
+    filename = sp[len(sp)-1]
+    try:
+        if(os.path.exists(file)):
+            return send_file(file, download_name=filename)
+        else:
+            raise Exception("File not found")
+    except Exception as e:
+        return str(e)
 
 @app.route('/arraysort', methods=['GET'])
 def arraysort():
@@ -13,16 +29,14 @@ def arraysort():
     if not 'algorithm' in json_data and 'inputValues' in json_data:
         response['message'] = "Invalid request"
         return jsonify(response)
-    sort_algo = json_data['algorithm']
-    arr = json_data['inputValues']
-    print(sort_algo, arr)
-    if not isinstance(sort_algo, str) or (not (isinstance(arr, list) and all(isinstance(val, int) for val in arr))): 
+    command = json_data['algorithm']
+    _inputValues = json_data['inputValues']
+    if not isinstance(command, str) or (not (isinstance(_inputValues, list) and all(isinstance(val, int) for val in _inputValues))): 
         response['message'] = "Invalid request"
         return jsonify(response)
     
     try:
-        int_list = [int(x) for x in arr]
-        file = ManimCsEngine(sort_algo, inputValues=arr).generate()
+        file = ManimCsEngine(command, inputValues=_inputValues, output_dir=_output_dir).generate()
     except Exception as e:
         response['message'] = str(e)
         return jsonify(response)
--- arrSortTestReq.sh
+++ arrSortTestReq.sh
@@ -1,1 +1,3 @@
 curl -X GET http://localhost:5000/arraysort -H "Content-Type: application/json" -H "Accept: application/json" -d '{"algorithm": "bubble-sort","inputValues":[11,23,15]}'
+
+curl -X GET http://localhost:5000/getfile -H "Content-Type: application/json" -H "Accept: application/json" -d '{"file": "/home/caden/developer/manim-cs-server/output/20230502-151807.mp4"}'
