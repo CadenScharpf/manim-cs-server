c7e2edd263c4d8ef60d9aee16d0128d9b2a1b991
Caden Scharpf <u16scharpf@gmail.com>
2023-05-02 19:14:19 -0700

    containerized app

--- arrSortTestReq.sh
+++ arrSortTestReq.sh
@@ -1,3 +1,3 @@
 curl -X GET http://localhost:5000/arraysort -H "Content-Type: application/json" -H "Accept: application/json" -d '{"algorithm": "bubble-sort","inputValues":[11,23,15]}'
 
-curl -X GET http://localhost:5000/getfile -H "Content-Type: application/json" -H "Accept: application/json" -d '{"file": "/home/caden/developer/manim-cs-server/output/20230502-151807.mp4"}'
+curl -o video.mp4 -X GET http://localhost:5000/getfile -H "Content-Type: application/json" -H "Accept: application/json" -d '{"file": "/app/output/20230503-020528.mp4"}'
